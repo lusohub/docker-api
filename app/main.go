73dc35955676f20834bb978e2e18c22d61ae162e
Martim Mourão <martimmourao@gmail.com>
2025-08-20 21:11:43 +0000

    First Commit

---
+++ app/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/martim-lusofona/games-api/db"
+	"github.com/martim-lusofona/games-api/rest"
+)
+
+func main() {
+	log.Println("Starting API...")
+
+	db.Connect()
+	db.Migrate()
+	db.Populate()
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+
+	})
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"ping": "pong",
+			"db":   rest.PingDB(),
+		})
+
+	})
+
+	router.GET("/games", rest.GetGames)
+
+	router.Run(":50007")
+}
